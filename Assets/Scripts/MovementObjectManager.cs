c0bb54df7ba842222387acaf9919b92bfdffb47e
DESKTOP-SF5LUQG\UVC-JYM <jym@uvc.co.kr>
2023-01-02 10:04:10 +0900

    상태 추가

--- Assets/Scripts/MovementObjectManager.cs
+++ Assets/Scripts/MovementObjectManager.cs
@@ -5,26 +5,47 @@ using TMPro;
 
 public class MovementObjectManager : MonoBehaviour
 {
+    public GameButtonManager gameButtonManager;
+
     public GameObject moveObjectPrefab;
+
     public GameObject movementObjectCollection;
-    public Transform spawnPoint;
+
+    public Transform dodger_spawnPoint;
+    public Transform maze_spawnPoint;
 
     public int spawnCount;
     public int arriveCount;
 
-    public TMP_Text spawnCountText;
-    public TMP_Text arriveCountText;
+    public TMP_Text dodger_spawnCountText;
+    public TMP_Text dodger_arriveCountText;
+
+    public TMP_Text maze_spawnCountText;
+    public TMP_Text maze_arriveCountText;
 
     private void Update()
     {
-        spawnCountText.text = spawnCount.ToString();
-        arriveCountText.text = arriveCount.ToString();
+        dodger_spawnCountText.text = spawnCount.ToString();
+        dodger_arriveCountText.text = arriveCount.ToString();
+
+        maze_spawnCountText.text = spawnCount.ToString();
+        maze_arriveCountText.text = arriveCount.ToString();
     }
     public void SpawnObject()
     {
-        Vector3 spawnPosition = new Vector3(spawnPoint.position.x, spawnPoint.position.y + 0.02f, spawnPoint.position.z);
-        GameObject moveObject = Instantiate(moveObjectPrefab, spawnPosition, Quaternion.identity, movementObjectCollection.transform);
-        spawnCount += 1;
+        if (gameButtonManager.gameType == GameType.DodgerGame)
+        {
+            Vector3 spawnPosition = new Vector3(dodger_spawnPoint.position.x, dodger_spawnPoint.position.y + 0.02f, dodger_spawnPoint.position.z);
+            GameObject moveObject = Instantiate(moveObjectPrefab, spawnPosition, Quaternion.identity, movementObjectCollection.transform);
+            spawnCount += 1;
+        }
+        else
+        {
+            Vector3 spawnPosition = new Vector3(maze_spawnPoint.position.x, maze_spawnPoint.position.y + 0.02f, maze_spawnPoint.position.z);
+            GameObject moveObject = Instantiate(moveObjectPrefab, spawnPosition, Quaternion.identity, movementObjectCollection.transform);
+            spawnCount += 1;
+        }
+        
     }
     public void DeleteObjects()
     {
